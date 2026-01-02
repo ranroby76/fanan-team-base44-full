53912d7ebced649e893bae7cdd0030d039841601
ranroby1 <ranroby76@gmail.com>
2026-01-02 14:36:01 +0000

    File changes

--- functions/getProducts.ts
+++ functions/getProducts.ts
@@ -5,28 +5,15 @@ Deno.serve(async (req) => {
         const base44 = createClientFromRequest(req);
         
         // Fetch all products using service role for public access
-        const products = await base44.asServiceRole.entities.Product.list('', 1000);
+        const products = await base44.asServiceRole.entities.Product.list();
         
         console.log('Fetched products count:', products.length);
+        console.log('Sample product:', JSON.stringify(products[0]));
         
         // Flatten products to include all fields at root level
         const flattenedProducts = products.map(p => ({
             id: p.id,
-            title: p.data.title,
-            short_description: p.data.short_description,
-            pack: p.data.pack,
-            formats: p.data.formats,
-            main_image: p.data.main_image,
-            gallery_images: p.data.gallery_images || [],
-            long_description: p.data.long_description,
-            download_links: p.data.download_links || [],
-            youtube_links: p.data.youtube_links || [],
-            price: p.data.price,
-            buy_link: p.data.buy_link,
-            demo_link: p.data.demo_link,
-            page_slug: p.data.page_slug,
-            supported_audio_formats: p.data.supported_audio_formats,
-            supported_video_formats: p.data.supported_video_formats,
+            ...p.data,
             created_date: p.created_date,
             updated_date: p.updated_date
         }));
