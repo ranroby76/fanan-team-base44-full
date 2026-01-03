364ac2f7afb12fdb887a2ae0815ff7e9de9f55df
ranroby1 <ranroby76@gmail.com>
2026-01-03 15:52:05 +0000

    File changes

--- src/pages/KittonStylist.jsx
+++ src/pages/KittonStylist.jsx
@@ -4,6 +4,7 @@ import ProductPage from "../components/ProductPage";
 export default function KittonStylist() {
   return (
     <ProductPage
+      slug="kitton-stylist"
       productName="Kitton Stylist"
       productDescription="Advanced Stylist Sampler"
       productImage="https://fananteam.com/images/kittonstylist1.jpg"
